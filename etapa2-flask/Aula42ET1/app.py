dec8393940fca2c10860102dfd8e0f30081323c4
JORGE_LUIS_DE_MORAES_LUCAS_PEREIRA <22400060@DELTAF52.FLORESTA>
2026-06-12 15:37:25 -0300

    directoryFix

--- etapa2-flask/Aula42ET1/app.py
+++ etapa2-flask/Aula42ET1/app.py
@@ -1,48 +1,32 @@
-from flask import Flask, render_template, request, make_response
-from funcs import User, Cadastro
+from flask import Flask, render_template, request
+from funcs import *
 
 app = Flask(__name__)
-
-def registerScreen(mensagem = None):
-    return render_template('register.html', mensagem = mensagem)
-def loginScreen(mensagem = None):
-    return render_template('login.html', mensagem = mensagem)
-
-def register():
-    username = request.form.get('user')
-    password = request.form.get('password')
-    newUser = User(username, password)
-    if not username or not password:
-        return make_response(registerScreen('Por favor, preencha todos os campos.'), 400)
-
-    mensagem, status = Cadastro.registrar(newUser)
-    return make_response(registerScreen(mensagem), status)
-
-def login():
-    username = request.form.get('user')
-    password = request.form.get('password')
-    user = User(username, password)
-    if not username or not password:
-        return make_response(loginScreen('Por favor, preencha todos os campos.'), 400)
-
-    mensagem, status = Cadastro.login(user)
-    return make_response(loginScreen(mensagem), status)
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
+def registerScreen():
+    return render_template('register.html')
+
+def register():
+        username =  request.form.get('user')
+        password = request.form.get('password')
+        newUser = User(username, password)
+        while not username or not password:
+            return 'Por favor, preencha todos os campos.', 400
+        
+        Cadastro.registrar(newUser)
+        mensagem = Cadastro.registrar(newUser)
+        return mensagem
 
 @app.route('/register', methods = ['GET', 'POST'])
-def registerPage():
+def registro():
     if request.method == 'POST':
         return register()
     return registerScreen()
-@app.route('/login', methods = ['GET', 'POST'])
-def loginPage():
-    if request.method == 'POST':
-        return login()
-    return loginScreen()    
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- etapa2-flask/Aula42ET1/funcs.py
+++ etapa2-flask/Aula42ET1/funcs.py
@@ -1,7 +1,7 @@
 import os
 import json
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+from flask import request
+BASE_DIR = r'H:\Python\etapa2\Aula42ET1'
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 PATH_JSON = os.path.join(DATA_DIR, 'usuarios.json')
 
@@ -33,29 +33,13 @@ class Cadastro:
             'user': user.user,
             'password' : user.password
         }
-        for i in dados: 
-            if i['user'] == userDict['user']:
-                return 'usuario já existe', 409
-
         dados.append(userDict)
-        with open(PATH_JSON, 'w', encoding='utf-8') as r:
+        
+        
+        with open(PATH_JSON, 'w', encoding= 'utf-8') as r:
             json.dump(dados, r, ensure_ascii=False, indent=4)
-        return 'usuario registrado com sucesso', 200
-
-    @staticmethod
-    def login(user : User):
-        dados = []
-        if not os.path.exists(PATH_JSON):
-            return 'usuario ou senha incorretos', 401
-        with open(PATH_JSON, 'r', encoding='utf-8') as f:
-            try:
-                dados = json.load(f)
-                if not isinstance(dados, list): 
-                    return 'usuario ou senha incorretos', 401
-            except json.JSONDecodeError:
-                return 'usuario ou senha incorretos', 401
+            return 'usuario registrado com sucesso'
             
-        for i in dados: 
-            if i['user'] == user.user and i['password'] == user.password:
-                return 'login bem sucedido', 200
-        return 'usuario ou senha incorretos', 401           
+
+    
+           
